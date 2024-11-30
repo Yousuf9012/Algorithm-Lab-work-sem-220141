b4e4cb9eb3b58d7ab304c6375a354e25f2361643
Md Yousuf  Ali Pk <165780362+Yousuf9012@users.noreply.github.com>
2024-11-30 19:53:06 +0600

    Update lab problem 1.cpp

--- lab problem 1.cpp
+++ lab problem 1.cpp
@@ -1,1 +1,39 @@
-lab problem 1.cpp
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    vector<int>v;
+    while(true)
+    {
+        int n;
+        cin>>n;
+        if(n==-1)
+            break;
+        v.push_back(n);
+   }
+    for(int x:v)
+    {
+        cout<<x<<" ";
+    }
+   // int p=v.size();
+   // cout<<"size="<<p;
+    int min=v.front();
+    int max=v.front();
+    int p=v.size();
+    for(int i=1;i<p;++i)
+    {
+        if(v[i]>max)
+        {
+            max=v[i];
+        }
+        if(v[i]<min)
+        {
+            min=v[i];
+        }
+       // {
+          //  cout<<v[i];
+        //}
+    }
+    cout<<"min="<<min;
+    cout<<"max="<<max;
+}
